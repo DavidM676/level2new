df18a3be1e522f05c897c4b23b171981b504aa70
DavidM676 <114095049+DavidM676@users.noreply.github.com>
2023-10-25 15:36:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function initialize() {
     const canvas = document.selectElementbyId("mycanvas");
     const ctx = canvas.getContext("2d");
+    const testImage = new Image(100,200)
+    testImage.src = "images/bat.png"
+    canvas.body.appendChild(testImage)
+    document.body.appendChild(testImage);
+    ctx.drawImage(testImage)
 }
